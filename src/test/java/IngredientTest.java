3f7dda932368250580b211705d5bf044b934feb6
Sergey Storozhilov <chidoshimeya@gmail.com>
2023-05-10 12:20:52 +0300

    again commit for reload files

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class IngredientTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IngredientTest {
+
+    @Mock
+    IngredientType ingredientType;
+
+    @Test
+    public void ingredientGetNameReturnsName(){
+        Ingredient ingredient = new Ingredient(ingredientType, "newIngredient", 2);
+        String expectedName = "newIngredient";
+
+        assertEquals("Имя ингридиента не совпадает!", expectedName, ingredient.getName());
+    }
+
+    @Test
+    public void ingredientGetPriceReturnsPrice(){
+        Ingredient ingredient = new Ingredient(ingredientType, "newIngredient", 2);
+        float expectedPrice = 2;
+
+        assertEquals("Цена ингридиента не совпадает!", expectedPrice, ingredient.getPrice(), 0);
+    }
 }
